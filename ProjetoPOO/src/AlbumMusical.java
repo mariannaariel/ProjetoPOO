e3d4ab7cd99026e886ae7e44f37e2e9a34dff388
Tarcisio Almeida <91756723+TarcisioAraujo7@users.noreply.github.com>
2022-10-31 13:29:46 -0300

    AlbumMusical + Musica

---
+++ ProjetoPOO/src/AlbumMusical.java
@@ -0,0 +1,9 @@
+public class AlbumMusical extends Produto{
+
+    private List<Musica> musicas = new Arraylist<Musica>();
+
+
+    public AlbumMusical(String nome, String descricao) {
+        super(nome, descricao);
+    }
+}
