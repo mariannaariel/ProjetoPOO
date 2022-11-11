167b7c17f53db28506d08cf3974710c12d97c51b
Marianna Ariel <mariannaariel@gmail.com>
2022-11-11 10:55:15 -0300

    Update Biblioteca.java

--- ProjetoPOO/src/Biblioteca.java
+++ ProjetoPOO/src/Biblioteca.java
@@ -11,12 +11,37 @@ public class Biblioteca {
         loja = new Loja();
         loja.addJogo(doom);
 
-        AlbumMusical albumDoom = new AlbumMusical("Album Doom", "Album do jogo Doom", 30);
+        Jogo bloodborne = new Jogo("Bloodborne", "Soulslike e horror", 50);
+        bloodborne.setConquista("Father Gascoigne foi derrotado", "Parabens! Você derrotou o primeiro BOSS.", 15);
+        bloodborne.setConquista("Você morreu 200 vezes", "Não se preocupe é normal", 30);
+        bloodborne.setConquista("Oh! Good Hunter", "Parabens! Você terminou o jogo", 100);
+
+        loja = new Loja();
+        loja.addJogo(bloodborne);
+
+
+        AlbumMusical albumDoom = new AlbumMusical("Álbum Doom", "Album do jogo Doom", 30);
         albumDoom.setMusica("The Only Thing They Fear Is You");
         albumDoom.setMusica("BFG Division");
         albumDoom.setMusica("Metahook");
 
         loja.addAlbum(albumDoom);
+
+        AlbumMusical albumBloodborne = new AlbumMusical("Bloodborne Original Soundtrack", "Album do jogo Bloordborne", 30);
+        albumBloodborne.setMusica("Omen");
+        albumBloodborne.setMusica("The Night Unfurls");
+        albumBloodborne.setMusica("Hunter's Dream");
+        albumBloodborne.setMusica("The Hunter");
+        albumBloodborne.setMusica("Cleric Beast");
+        albumBloodborne.setMusica("Blood-starved Beast");
+        albumBloodborne.setMusica("Watchers");
+        albumBloodborne.setMusica("Hail The Nightmare");
+        albumBloodborne.setMusica("Darkbeast");
+        albumBloodborne.setMusica("The Witch Of Hemwick");
+        albumBloodborne.setMusica("Rom, The Vacuous Spider");
+        albumBloodborne.setMusica("Moonlit Melody");
+
+        loja.addAlbum(albumBloodborne);
 
     }
 
